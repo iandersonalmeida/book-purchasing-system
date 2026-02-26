355ebe0aa02f39b152d1b5416aab1898b4beb660
unknown <ianderson.almeida@gmail.com>
2026-02-26 15:45:26 +0100

    test: refactor BookServiceServiceTest to use Mockito only

--- backend-side/bookservice/src/test/java/ian/projects/backend/service/BookServiceServiceTest.java
+++ backend-side/bookservice/src/test/java/ian/projects/backend/service/BookServiceServiceTest.java
@@ -5,15 +5,19 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import ian.projects.backend.BookServiceApplication;
 import ian.projects.backend.domain.Book;
 import ian.projects.backend.repository.BookServiceRepository;
+import ian.projects.backend.service.BookServiceService;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.util.Collections;
 import java.util.List;
+
 
 @ExtendWith(MockitoExtension.class)
 public class BookServiceServiceTest {
