99d264c1ed8d187816a89985b15611863c495107
unknown <ianderson.almeida@gmail.com>
2026-07-17 17:25:46 +0200

    US-001 (feat): validate ISBN-13 check digit

--- backend-side/bookservice/src/main/java/ian/projects/backend/domain/ISBN.java
+++ backend-side/bookservice/src/main/java/ian/projects/backend/domain/ISBN.java
@@ -1,16 +1,21 @@
 package ian.projects.backend.domain;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 /**
  * Represents the ISBN of a specific book edition.
  *
  * The ISBN is immutable and is normalized during creation.
  */
-public class ISBN {
+public final class ISBN {
 
 	private final String value;
 	private final String normalizedValue;
 	private static final int ISBN_LENGTH_10 = 10;
 	private static final int ISBN_LENGTH_13 = 13;
+	public static final int DIVISOR = 10;
+	public static final int MINUEND = 10;
 
 	public ISBN(String value) {
 		this.value = value;
@@ -29,18 +34,89 @@ public class ISBN {
 		// After normalization, an ISBN must contain only digit characters.
 		if (!normalizedValue.matches("[0-9]+")) {
 			throw new IllegalArgumentException("ISBN creation rejected. ISBN must contain only digits.");
-		}		
-		
-		if(normalizedValue.length() != ISBN_LENGTH_10 && normalizedValue.length() != ISBN_LENGTH_13) {
+		}
+
+		if (normalizedValue.length() != ISBN_LENGTH_10 && normalizedValue.length() != ISBN_LENGTH_13) {
 			throw new IllegalArgumentException("ISBN creation rejected. ISBN muss contain exactly 10 or 13 digits.");
-		}			
-	
-	} 
+		}
+
+		verifyCheckDigit();
+
+	}
+
 	public String getValue() {
 		return normalizedValue;
 	}
-	
-	private final String isbnCreationMessage() {
-		return "ISBN Created Successfully!";
+
+	private void verifyCheckDigit() {
+
+		// Extract digit
+		String extractedDigit = normalizedValue.substring(12, 13);
+
+		// Convert to int
+		int enteredDigit = Integer.parseInt(extractedDigit);
+
+		String first12Digits = normalizedValue.substring(0, 12);
+		//System.out.println("First 12 digits: " + first12Digits);
+		
+		//Convert to int
+		int[] digits = first12Digits.chars().map(Character::getNumericValue).toArray();
+		//System.out.println("Digits: " + Arrays.toString(digits));
+		
+		//Extract the values ​​of the even indices
+		int[] evenIndices = IntStream.range(0, digits.length).filter(i -> i % 2 == 0).map(i -> digits[i]).toArray();
+		//System.out.println("Even Indices Values: " + Arrays.toString(evenIndices));
+		
+		//Extract the values ​​of the odd indices
+		int[] oddIndices = IntStream.range(0, digits.length).filter(i -> i % 2 != 0).map(i -> digits[i]).toArray();
+		//System.out.println("Odd Indices Values: " + Arrays.toString(oddIndices));
+
+		// Computed values of even indices
+		int[] evenIndexedDigits = new int[6];
+		for (int i = 0; i < evenIndices.length; i++) {
+			evenIndexedDigits[i] = evenIndices[i] * 1;
+			//String computedEvenvalues = Arrays.toString(evenIndexedDigits);
+			//System.out.println("Computed values of even indices: " + computedEvenvalues);
+		}
+
+		// Computed values of odd indices
+		int[] oddIndexedDigits = new int[6];
+		for (int i = 0; i < oddIndices.length; i++) {
+			oddIndexedDigits[i] = oddIndices[i] * 3;
+			//String computedOddvalues = Arrays.toString(oddIndexedDigits);
+			//System.out.println("Computed values of odd indices: " + computedOddvalues);
+		}
+
+		// Sum of digits
+		int digitsSum = 0;
+		for (int i = 0; i < evenIndexedDigits.length; i++) {
+			digitsSum += evenIndexedDigits[i] + oddIndexedDigits[i];
+		}
+
+		System.out.println("Result: " + digitsSum);
+
+		// Divide sum of digits by 10
+		int remainder = digitsSum % DIVISOR;
+		System.out.println("remainder: " +remainder);
+
+		// Take the remainder and subtract it from 10 to obtain the check digit.
+		int checkDigit = MINUEND - remainder;
+		System.out.println("Check Digit: " + checkDigit);
+		System.out.println("Entered Digit: " + enteredDigit);
+		
+		//special case if check digit is 10
+		if(checkDigit == 10) {
+			checkDigit = 0;
+			System.out.println("Check digit changed from 10 to 0. Check digit = " +checkDigit);
+		}
+		
+		
+
+		if (checkDigit != enteredDigit) {
+			throw new IllegalArgumentException("ISBN creation rejected. Invalid Digit.");
+		}	
+		
+		System.out.println("ISBN created successfully.");
+
 	}
 }
--- backend-side/bookservice/src/test/java/ian/projects/backend/domain/ISBNTests.java
+++ backend-side/bookservice/src/test/java/ian/projects/backend/domain/ISBNTests.java
@@ -1,6 +1,7 @@
 package ian.projects.backend.domain;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import org.slf4j.LoggerFactory;
@@ -11,7 +12,9 @@ public class ISBNTests {
 	private static final Logger logger = LoggerFactory.getLogger(ISBN.class);
 
 	@Test
+	@DisplayName("Should reject null value")
 	void shouldRejectNullValue() {
+		logger.info("TEST: Should reject null value");
 
 		// Given a invalid value
 		String value = null;
@@ -26,7 +29,9 @@ public class ISBNTests {
 	}
 
 	@Test
+	@DisplayName("Should reject blank value")
 	void shouldRejectBlankValue() {
+		logger.info("TEST: Should reject blank value");
 		String value = "\t  ";
 
 		Exception isbnCreation = Assertions.assertThrows(IllegalArgumentException.class, () -> new ISBN(value));
@@ -37,7 +42,9 @@ public class ISBNTests {
 	}
 
 	@Test
+	@DisplayName("Should normalize hyphenated ISBN")
 	void shouldNormalizeHyphenatedISBN() {
+		logger.info("TEST: Should normalize hyphenated ISBN");
 		// Given
 		String value = "978-0-13-235088-4";
 
@@ -50,7 +57,9 @@ public class ISBNTests {
 	}
 
 	@Test
+	@DisplayName("Should reject ISBN containing non digit characters")
 	void shouldRejectISBNContainingNonDigitCharacters() {
+		logger.info("TEST: Should reject ISBN containing non digit characters");
 		// Given
 		String value = "978@132350884";
 
@@ -63,7 +72,10 @@ public class ISBNTests {
 	}
 
 	@Test
+	@DisplayName("Should reject ISBN not containing 10 or 13 digits")
 	void shouldRejectISBNNotContaining10Or13Digits() {
+		logger.info("TEST: Should reject ISBN not containing 10 or 13 digits");
+		
 
 		// Given
 		String value = "123456789";
@@ -72,8 +84,59 @@ public class ISBNTests {
 		Exception isbnCreation = Assertions.assertThrows(IllegalArgumentException.class, () -> new ISBN(value));
 
 		// Then
-		Assertions.assertEquals("ISBN creation rejected. ISBN muss contain exactly 10 or 13 digits.", isbnCreation.getMessage());
+		Assertions.assertEquals("ISBN creation rejected. ISBN muss contain exactly 10 or 13 digits.",
+				isbnCreation.getMessage());
 		logger.info("ISBN creation rejected :" + isbnCreation);
 	}
+
+	@Test
+	@DisplayName("Should verify invalid check digit")
+	void shouldVerifyInvalidCheckDigit() {
+		logger.info("TEST: Should verify invalid check digit");
+		
+
+		// Given
+		String invalidValue = "9780132350885";
+
+		// When
+		Exception isbnCreation = Assertions.assertThrows(IllegalArgumentException.class, () -> new ISBN(invalidValue));
+
+		// Then
+		Assertions.assertEquals("ISBN creation rejected. Invalid Digit.", isbnCreation.getMessage());
+		logger.info("ISBN creation rejected :" + isbnCreation);
+	}
+	
+	@Test
+	@DisplayName("Should verify valid check digit")
+	void shouldVerifyValidCheckDigit() {
+		logger.info("TEST: Should verify valid check digit");
+		
+		//Given
+		String validValue = "9780132350884";
+		
+		//When
+		ISBN isbn = new ISBN(validValue);
+		
+		//Then
+		Assertions.assertEquals("9780132350884", isbn.getValue());
+	}
+	
+	@Test
+	@DisplayName("Should create ISBN with check digit zero")
+	void shouldCreateISBNWithCheckDigitZero() {
+		
+		logger.info("TEST: Should create ISBN with check digit zero");
+		
+		
+		//Given
+		String value = "978-0-201-63361-0";
+		
+		//When
+		ISBN isbn = new ISBN(value);
+		
+		//Then
+		Assertions.assertEquals("9780201633610", isbn.getValue());
+	}
+	
 
 }
